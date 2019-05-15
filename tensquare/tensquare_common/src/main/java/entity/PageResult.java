c602e1bd34ef1190dd3e1f9559fb072a62e5b262
jk416 <zhouzilong1994@126.com>
2019-05-15 18:43:40 +0800

    十次方

---
+++ tensquare/tensquare_common/src/main/java/entity/PageResult.java
@@ -0,0 +1,36 @@
+package entity;
+
+import java.util.List;
+
+/**
+ * @author: jk416
+ * @create: 2019-05-13 19:08
+ */
+public class PageResult <T> {
+    private long total;
+    private List<T> rows;
+
+    public PageResult() {
+    }
+
+    public PageResult(long total, List<T> rows) {
+        this.total = total;
+        this.rows = rows;
+    }
+
+    public long getTotal() {
+        return total;
+    }
+
+    public void setTotal(long total) {
+        this.total = total;
+    }
+
+    public List<T> getRows() {
+        return rows;
+    }
+
+    public void setRows(List<T> rows) {
+        this.rows = rows;
+    }
+}
---
+++ tensquare/tensquare_common/src/main/java/entity/StatusCode.java
@@ -0,0 +1,9 @@
+package entity;
+
+/**
+ * @author: jk416
+ * @create: 2019-05-13 19:10
+ */
+public class StatusCode {
+
+}
